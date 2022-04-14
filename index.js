f56c00719d6b866b4d71c918351107cf4f36a613
pedro.caricari <pedro.caricari@bg.com.bo>
2022-04-14 02:09:12 -0400

    update index

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const app = require('./src/app')
+const morgan = require('morgan')
 
+const app = require('./src/app')
 const PORT = 8000
 
+app.use(morgan('dev'))
 app.listen(PORT, () => {
     console.log(new Date())
     console.log(`Server runnning on http://localhost:${PORT}/`);
